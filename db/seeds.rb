f7cc04d171fe3e5661fb0ac8bd0ed36b96f23af0
RicardoMartinez <ricardoemartine@hotmail.com>
2021-02-18 18:06:17 +0100

    Cocktail app created with rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Ingredient.create(name: "Mint")
+# Ingredient.create(name: "Vodka")
+# Ingredient.create(name: "Rhum")
+# Ingredient.create(name: "Tequila")
+# Ingredient.create(name: "Orange Juice")
+# Ingredient.create(name: "Tomato Juice")
+
+require 'open-uri'
+require 'json'
+
+puts "Cleaning database..."
+Ingredient.destroy_all
+
+puts "Reading ingredient names..."
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients['drinks'].each do |i|
+  ingredient  = Ingredient.create!(
+    name: i['strIngredient1']
+  )
+  puts "Ingredient #{ingredient.name} succesfully created"
+end
+# puts "All the ingredients successfully created!"
+# end
